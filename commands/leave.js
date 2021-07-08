2ea279163914276c1e425f781f2606ef8c51337b
alrafiabdullah <abdullah34alrafi@gmail.com>
2021-07-09 01:00:58 +0600

    parameter updated

--- commands/leave.js
+++ commands/leave.js
@@ -1,7 +1,7 @@
 module.exports = {
     name: "leave",
     description: "This is a leave command",
-    async execute(message, args) {
+    async execute(message, args, cmd, client, Discord) {
         const voiceChannel = message.member.voice.channel;
         if (!voiceChannel) {
             return message.channel.send(`${message.member.displayName}, you need to be in a voice channel to execute this command!`);
--- commands/ping.js
+++ commands/ping.js
@@ -1,7 +1,7 @@
 module.exports = {
     name: "ping",
     description: "This is a ping command",
-    execute(message, client) {
+    execute(message, args, cmd, client, Discord) {
         message.channel.send(`${client.ws.ping}ms`);
     }
 }
